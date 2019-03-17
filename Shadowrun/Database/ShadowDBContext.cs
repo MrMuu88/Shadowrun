98ad5f50abac24222cdb09d7a9f348bd289f5522
Simon Tamas Jozsef <simontjozsef@gmail.com>
2019-03-17 15:50:34 +0100

    Added EF Core Project for database access

---
+++ Shadowrun/Database/ShadowDBContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shadowrun.Database {
+	public class ShadowDBContext : DbContext {
+		#region fields and Properties #############################################################
+		private string ConnectionString;
+
+		DbSet<Skill> Skills { get; set; }
+		DbSet<Specialization> Specializations { get; set; }
+
+		DbSet<Meele> MeeleWeapons { get; set; }
+		DbSet<Projectile> ProjectileWeapons{ get; set; }
+		DbSet<FireArm> FireArms{ get; set; }
+		DbSet<Explosive> Explosives { get; set; }
+
+		#endregion
+
+		#region Methods ###########################################################################
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			optionsBuilder.UseMySql(ConnectionString);
+			base.OnConfiguring(optionsBuilder);	
+		}
+
+		#endregion
+
+		#region Ctors #############################################################################
+
+		public ShadowDBContext(string Constr) {
+			ConnectionString = Constr;
+			Database.EnsureCreated();
+		}
+		#endregion
+	}
+}
